10b25d99315dc93b8b5f3fd55dcfdf84a310b04a
mikayladalton2 <103148825+mikayladalton2@users.noreply.github.com>
2022-05-01 17:37:42 -0700

    fixed test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -6,7 +6,7 @@ public class SkillDemoTest {
 @Test
 public void testAdd(){
 
-    assertEquals("These should be equal",3, SkillDemo.add(2,4)); //should fail
+    assertEquals("These should be equal",6, SkillDemo.add(2,4)); //should fail
 
 }
 
